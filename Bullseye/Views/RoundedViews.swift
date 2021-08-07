77e50247b673371150fef5d486f56c267f1b2ba8
yyriley <yyriley0124@gmail.com>
2021-08-07 00:24:04 -0500

    Added background views and test file for shapes

---
+++ Bullseye/Views/RoundedViews.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedViews.swift
+//  Bullseye
+//
+//  Created by Yrua Riley on 8/6/21.
+//
+
+import SwiftUI
+
+struct RoundedViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoundedViews_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedViews()
+    }
+}
