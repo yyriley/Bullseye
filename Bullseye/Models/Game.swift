c12b79add8ef6284245bb55011c284c87e622e2a
yyriley <yyriley0124@gmail.com>
2021-08-08 04:10:07 -0500

    Added startNewRound function

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -13,4 +13,11 @@ struct Game {
     var round = 1
     
     func points(sliderValue: Int) -> Int {100 - abs(sliderValue - self.target)}
+  
+  //mutating because it changes the values inside the struct
+  mutating func startNewRound(points : Int) {
+    score += points
+    round += 1;
+    target = Int.random(in: 1...100)
+  }
 }
--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -31,4 +31,10 @@ class BullseyeTests: XCTestCase {
         let score = game.points(sliderValue: guess)
         XCTAssertEqual(score, 95)
     }
+  
+  func testNewRound() {
+    game.startNewRound(points: 100)
+      XCTAssertEqual(game.score, 100)
+      XCTAssertEqual(game.round, 2)
+  }
 }
