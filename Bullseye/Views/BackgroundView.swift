d91e35439542618d65e6aabbc7896e337cf21fe1
Yrua Riley <yyriley0124@gmail.com>
2021-08-17 05:48:35 -0500

    Added Environment property for color scheme

--- Bullseye/Views/BackgroundView.swift
+++ Bullseye/Views/BackgroundView.swift
@@ -60,16 +60,19 @@ struct BottomView: View {
 }
 
 struct RingsView: View {
+  @Environment(\.colorScheme) var colorScheme
+  
   var body: some View {
     ZStack{
       Color("BackgroundColor")
         .edgesIgnoringSafeArea(.all)
       ForEach(1..<6) { ring in
        let size = CGFloat(ring * 100)
+        let opacity = colorScheme == .dark ? 0.1 : 0.3
         Circle()
           .stroke(lineWidth: 20)
           .fill(
-            RadialGradient(gradient: Gradient(colors: [Color("RingsColor").opacity(0.3 * 0.8), Color("RingsColor").opacity(0)]), center: .center, startRadius: 100, endRadius: 300)
+            RadialGradient(gradient: Gradient(colors: [Color("RingsColor").opacity(opacity), Color("RingsColor").opacity(0)]), center: .center, startRadius: 100, endRadius: 300)
           )
           .frame(width: size, height:size)
         
